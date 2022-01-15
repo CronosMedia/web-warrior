1ac89d814b0234a9b58b3b53d2b9b00a7187a88d
CronosMedia <cronosdesignro@gmail.com>
2022-01-15 05:35:35 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime qui
+          voluptate soluta accusantium ex maiores. Voluptatem libero quam porro
+          tempora sint quisquam esse, vel magnam repellat qui. Harum, at
+          deleniti.
+        </p>
+      </div>
+    </Layout>
+  )
+}
